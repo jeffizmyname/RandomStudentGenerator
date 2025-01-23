b4229b4ac41d69de96b1e99d74038962a0dd908a
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-24 00:34:12 +0100

    for now its just random

--- RandomStudentGenerator/CustomControls/HappyNumber.xaml.cs
+++ RandomStudentGenerator/CustomControls/HappyNumber.xaml.cs
@@ -5,5 +5,8 @@ public partial class HappyNumber : ContentView
 	public HappyNumber()
 	{
 		InitializeComponent();
-	}
+        Random random = new Random();
+        int number = random.Next(1, 40);
+        happyNumber.Text = number.ToString();
+    }
 }
