ec9085f398d657b8d49c2e78c4f06d7974d1f431
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-24 00:33:31 +0100

    OMGGG its a new fontt wow

--- RandomStudentGenerator/MauiProgram.cs
+++ RandomStudentGenerator/MauiProgram.cs
@@ -11,8 +11,9 @@ namespace RandomStudentGenerator
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Gabarito-VariableFont_wght.ttf", "Gabarito")
+                        .AddFont("OpenSans-Regular.ttf", "OpenSans-Regular")
+                        .AddFont("OpenSans-SemiBold.ttf", "OpenSans-SemiBold");
                 });
 
 #if DEBUG
