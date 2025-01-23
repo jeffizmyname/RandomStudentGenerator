7c1552706528c65f1937acf87e9681491e8f4c9e
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-24 00:35:13 +0100

    view model init

--- RandomStudentGenerator/CustomControls/StudentsList.xaml.cs
+++ RandomStudentGenerator/CustomControls/StudentsList.xaml.cs
@@ -1,9 +1,21 @@
+using RandomStudentGenerator.ViewModels;
+using System.Diagnostics;
+using static RandomStudentGenerator.StorageHandlers.StorageHandler;
+
 namespace RandomStudentGenerator.CustomControls;
 
 public partial class StudentsList : ContentView
 {
-	public StudentsList()
+	public ClassViewModel _classViewModel { get; set; }
+    public StudentsList()
 	{
 		InitializeComponent();
-	}
+        _classViewModel = new ClassViewModel();
+        studentsList.BindingContext = _classViewModel;
+    }
+
+    public void UpdateData(string className)
+    {
+        _classViewModel.setClass(ReadClass(className));
+    }
 }
