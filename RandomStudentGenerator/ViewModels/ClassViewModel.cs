0938bff90aee6ba556f663a4758a959983fd6435
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-24 00:37:00 +0100

    view model for dispalying studentsfrom current class

---
+++ RandomStudentGenerator/ViewModels/ClassViewModel.cs
@@ -0,0 +1,27 @@
+﻿using RandomStudentGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomStudentGenerator.ViewModels
+{
+    public class ClassViewModel
+    {
+        public Class Class { get; set; }
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+
+        public ClassViewModel()
+        {
+        }
+
+        public void setClass(Class c)
+        {
+            Class = c;
+            c.students.ToList().ForEach(Students.Add);
+        }
+    }
+}
