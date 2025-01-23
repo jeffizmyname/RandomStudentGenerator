107504ae43c53694d8c64d3cc2eb639c588867e6
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-24 00:36:28 +0100

    add class and read class methodes

--- RandomStudentGenerator/StorageHandlers/StorageHandler.cs
+++ RandomStudentGenerator/StorageHandlers/StorageHandler.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using RandomStudentGenerator.CustomControls;
+using RandomStudentGenerator.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +21,56 @@ namespace RandomStudentGenerator.StorageHandlers
             if(!Directory.Exists(rootPath)) Directory.CreateDirectory(rootPath);
         }
 
-        public static void AddClass()
+        public static void AddClass(string filePath)
         {
+            Init();
+            var fileName = Path.GetFileName(filePath);
+            var destPath = Path.Combine(rootPath, fileName);
+            File.Copy(filePath, destPath, true);
+        }
 
+        public static List<string> ReadClassNames()
+        {
+            Init();
+            var files = Directory.GetFiles(rootPath);
+            return files
+                 .Select(f => Path.GetFileNameWithoutExtension(f) ?? string.Empty)
+                 .ToList();
+        }
+
+        public static Class ReadClass(string className)
+        {
+            Init();
+            var filePath = Path.Combine(rootPath, className + ".csv");
+            if (!File.Exists(filePath)) return new Class(":<", new List<Student>());
+
+            var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                DetectDelimiter = true,
+                HasHeaderRecord = false
+            };
+
+            using (var stream = new StreamReader(filePath))
+            using (var csv = new CsvReader(stream, config))
+            {
+                var records = csv.GetRecords<Student>();
+                Debug.WriteLine(records);
+                List<Student> temp = new List<Student>();
+
+                bool workaround = true;
+                foreach (var record in records)
+                {
+                    //Debug.WriteLine("record: " + record.Name);
+                    if (workaround)
+                    {
+                        workaround = false;
+                        continue;
+                    }
+                    temp.Add(record);
+                }
+
+                return new Class(className, temp);
+            }
         }
     }
 }
