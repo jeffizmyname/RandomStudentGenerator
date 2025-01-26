d84713b5d4dba1a4cc2040a1824d965090fee242
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-26 23:43:09 +0100

    add student  methode

--- RandomStudentGenerator/StorageHandlers/StorageHandler.cs
+++ RandomStudentGenerator/StorageHandlers/StorageHandler.cs
@@ -1,5 +1,6 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
+using RandomStudentGenerator.CsvMaps;
 using RandomStudentGenerator.CustomControls;
 using RandomStudentGenerator.Models;
 using System;
@@ -57,33 +58,43 @@ namespace RandomStudentGenerator.StorageHandlers
             var filePath = Path.Combine(rootPath, className + ".csv");
             if (!File.Exists(filePath)) return new Class(":<", new List<Student>());
 
-            var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 DetectDelimiter = true,
-                HasHeaderRecord = false
+                HasHeaderRecord = true,
+                PrepareHeaderForMatch = args => args.Header.ToLower(),
+                HeaderValidated = null, 
+                MissingFieldFound = null
             };
 
             using (var stream = new StreamReader(filePath))
             using (var csv = new CsvReader(stream, config))
             {
-                var records = csv.GetRecords<Student>();
+                var records = csv.GetRecords<Student>().ToList();
                 Debug.WriteLine(records);
-                List<Student> temp = new List<Student>();
-
-                bool workaround = true;
-                foreach (var record in records)
-                {
-                    //Debug.WriteLine("record: " + record.Name);
-                    if (workaround)
-                    {
-                        workaround = false;
-                        continue;
-                    }
-                    temp.Add(record);
-                }
-
-                return new Class(className, temp);
+                return new Class(className, records);
             }
+        }
+
+        public static void AddStudent(Student student, string className)
+        {
+            Init();
+            var filePath = Path.Combine(rootPath, className + ".csv");
+            if (!File.Exists(filePath)) return;
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                DetectDelimiter = true,
+                HasHeaderRecord = true,
+            };
+
+            using (var stream = new StreamWriter(filePath, append: true))
+            using (var csv = new CsvWriter(stream, config))
+            {
+                csv.Context.RegisterClassMap<StudentMap>();
+                csv.WriteRecord(student);
+            }
+
         }
     }
 }
