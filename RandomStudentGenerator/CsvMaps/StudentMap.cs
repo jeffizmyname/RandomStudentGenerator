4e29f725c0b0e8675d68e7e4e37f6c53bb0e43e2
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-26 23:45:30 +0100

    map

---
+++ RandomStudentGenerator/CsvMaps/StudentMap.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+using RandomStudentGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomStudentGenerator.CsvMaps
+{
+    public class StudentMap : ClassMap<Student>
+    {
+        public StudentMap() 
+        {
+            Map(m => m.Id).Name("Id");
+            Map(m => m.Name).Name("Name");
+            Map(m => m.Surname).Name("Surname");
+        }
+    }
+}
