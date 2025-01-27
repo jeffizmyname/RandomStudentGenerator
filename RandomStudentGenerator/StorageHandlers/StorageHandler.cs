708b43dc54df713b5982ade7ab1e7a306e06e78e
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-27 22:54:11 +0100

    methode for class size

--- RandomStudentGenerator/StorageHandlers/StorageHandler.cs
+++ RandomStudentGenerator/StorageHandlers/StorageHandler.cs
@@ -96,5 +96,26 @@ namespace RandomStudentGenerator.StorageHandlers
             }
 
         }
+
+        public static int getClassSize(string className)
+        {
+            Init();
+            var filePath = Path.Combine(rootPath, className + ".csv");
+            if (!File.Exists(filePath)) return -1;
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                DetectDelimiter = true,
+                HasHeaderRecord = true,
+                PrepareHeaderForMatch = args => args.Header.ToLower(),
+                HeaderValidated = null,
+                MissingFieldFound = null
+            };
+            using (var stream = new StreamReader(filePath))
+            using (var csv = new CsvReader(stream, config))
+            {
+                var records = csv.GetRecords<Student>().ToList();
+                return records.Count;
+            }
+        }
     }
 }
