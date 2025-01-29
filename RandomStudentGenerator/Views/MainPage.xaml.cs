077911ec8f3c12d32d446a9dc104f38cdef4ed87
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-01-29 22:42:05 +0100

    remuvd wisteew

--- RandomStudentGenerator/Views/MainPage.xaml.cs
+++ RandomStudentGenerator/Views/MainPage.xaml.cs
@@ -6,7 +6,6 @@
         {
             InitializeComponent();
             classSelector.SelectorChanged += (s, data) => studentList.UpdateData(data);
-            classSelector.SelectorChanged += (s, data) => numberGenerator.UpdateData(data);
             classSelector.newClass += (s, data) => studentList.CreateClass(data);
         }
 
