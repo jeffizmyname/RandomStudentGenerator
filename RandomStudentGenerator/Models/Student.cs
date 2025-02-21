c1259bbf77f3e469793149a74d9b95f87df9e500
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-02-22 00:47:15 +0100

    presence stuff

--- RandomStudentGenerator/Models/Student.cs
+++ RandomStudentGenerator/Models/Student.cs
@@ -27,14 +27,21 @@ namespace RandomStudentGenerator.Models
             Id = id;
             Name = name;
             Surname = surname;
+            Presences = new List<Presence>();
             currentPresence = new Presence(DateTime.Now, false);
         }
 
-        public void savePresence()
+        public void savePresence(bool isPresent)
         {
-            currentPresence.date = DateTime.Now;
+            currentPresence = new Presence(DateTime.Now, isPresent);
             Presences.Add(currentPresence);
-            currentPresence = new Presence(DateTime.Now, false); // ??
         }
+
+        public Presence GetPresenceForDate(DateTime selectedDate)
+        {
+            return Presences.FirstOrDefault(p => p.date.Date == selectedDate.Date)
+                   ?? new Presence(selectedDate, false);
+        }
+
     }
 }
