e28ed768a87332ace503025c3c103e78b6be7e81
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-02-22 00:46:40 +0100

    student presence

--- RandomStudentGenerator/ViewModels/ClassViewModel.cs
+++ RandomStudentGenerator/ViewModels/ClassViewModel.cs
@@ -1,4 +1,5 @@
-﻿using RandomStudentGenerator.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RandomStudentGenerator.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using static RandomStudentGenerator.StorageHandlers.StorageHandler;
 
 namespace RandomStudentGenerator.ViewModels
@@ -15,8 +17,22 @@ namespace RandomStudentGenerator.ViewModels
         public Class Class { get; set; }
         public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
 
+        private DateTime _selectedDate = DateTime.Today;
+        public DateTime SelectedDate
+        {
+            get => _selectedDate;
+            set
+            {
+                _selectedDate = value;
+                UpdatePresencesForSelectedDate();
+            }
+        }
+
+        public ICommand deleteStudentCommand { get; }
+
         public ClassViewModel()
         {
+            deleteStudentCommand = new Command<Student>(deleteStudent);
         }
 
         public void newClass(string className)
@@ -30,6 +46,13 @@ namespace RandomStudentGenerator.ViewModels
             if(Class == null) return;
             var student = new Student(0, name, surname);
             Class.students.Add(student);
+            sortStudents();
+            AddStudent(Students.ToList(), Class.className);
+        }
+
+        private void deleteStudent(Student student)
+        {
+            Class.students.Remove(student);
             sortStudents();
             AddStudent(Students.ToList(), Class.className);
         }
@@ -56,6 +79,25 @@ namespace RandomStudentGenerator.ViewModels
             {
                 sortedStudents[i].Id = i + 1;
                 Students.Add(sortedStudents[i]);
+            }
+        }
+
+        public void setStudentPresence(Student student, bool isPresent)
+        {
+            if (student == null) return;
+
+            var presence = student.GetPresenceForDate(SelectedDate);
+            presence.isPresent = isPresent;
+
+            if (!student.Presences.Contains(presence))
+                student.Presences.Add(presence);
+        }
+
+        private void UpdatePresencesForSelectedDate()
+        {
+            foreach (var student in Students)
+            {
+                student.CurrentPresence = student.GetPresenceForDate(SelectedDate);
             }
         }
     }
