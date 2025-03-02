253bb2039f7d9070dc75c37104d37fb9a6c14199
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2025-03-02 14:51:11 +0100

    srs idk

--- RandomStudentGenerator/MauiProgram.cs
+++ RandomStudentGenerator/MauiProgram.cs
@@ -15,7 +15,7 @@ namespace RandomStudentGenerator
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
